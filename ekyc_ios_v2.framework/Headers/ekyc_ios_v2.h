7b09ccf6c890aeebd6bac40cb89b943177dc1818
Ikhlas <ikhlas.risandy@sehatq.com>
2020-06-09 19:39:19 +0700

    add framework

---
+++ ekyc_ios_v2.framework/Headers/ekyc_ios_v2.h
@@ -0,0 +1,19 @@
+//
+//  ekyc_ios_v2.h
+//  ekyc-ios-v2
+//
+//  Created by Eka Praditya Giovanni Kariim on 24/04/2020.
+//  Copyright © 2020 Teravin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ekyc_ios_v2.
+FOUNDATION_EXPORT double ekyc_ios_v2VersionNumber;
+
+//! Project version string for ekyc_ios_v2.
+FOUNDATION_EXPORT const unsigned char ekyc_ios_v2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ekyc_ios_v2/PublicHeader.h>
+
+
